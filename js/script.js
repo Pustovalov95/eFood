68de846663e2998a743fc3d41a47dc33efa1ba6b
Pustovalov95 <88744673+Pustovalov95@users.noreply.github.com>
2021-12-18 15:02:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const   btn = document.querySelector('.header__nav-burger'),
+        menu =document.querySelector('.menu');
+
+function showAndHideMenu() {
+    menu.classList.toggle('active');
+    btn.classList.toggle('active');
+}
+
+btn.addEventListener('click', () => {
+    showAndHideMenu();
+});
+$(document).ready(function(){
+    $('.popular__burgers_wrapper').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/popular/arrow-left.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/popular/arrow-right.png" alt=""></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      });
+  });
+  $(document).ready(function(){
+    $('.feedback-wrapper').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/feedback/ArrowLeft.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/feedback/ArrowRight.png" alt=""></button>'
+      });
+  });
